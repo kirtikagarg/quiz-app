2f35419c33d66c7eb60aa747f4d45e6872e5e549
kirtikagarg <47203126+kirtikagarg@users.noreply.github.com>
2021-03-18 16:44:39 +0530

    Add files via upload

--- myquiz/js1.js
+++ myquiz/js1.js
@@ -2,7 +2,7 @@
 let questions = [
     {
     numb: 1,
-    question: "What does HTML stand for?",
+    question: "kanu What does HTML stand for?",
     answer: "Hyper Text Markup Language",
     options: [
       "Hyper Text Preprocessor",
@@ -70,68 +70,61 @@ let questions = [
   //   ]
   // },
 ];
-
-
 // creating an array and passing the number, questions, options, and answers
 let questionsA = [
     {
     numb: 1,
-    questionA: " 	There is 60% increase in an amount in 6 years at simple interest. What will be the compound interest of Rs. 12,000 after 3 years at the same rate??",
-    answerA: " Option C",
+    questionA: "  What does HTML stand for?",
+    answerA: "Hyper Text Markup Language",
     options: [
-      "A.	Rs. 2160",
-      "B.	Rs. 3120",
-      "C.	Rs. 3972",
-      "D.	Rs. 6240"
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
     ]
   },
     {
     numb: 2,
-    questionA: "	What is the rate of compound interest?"
-    "I. The principal was invested for 4 years."
-
-    "II. The earned interest was Rs. 1491.",
-    answerA: "Option D",
+    questionA: "What does CSS stand for?",
+    answerA: "Cascading Style Sheet",
     options: [
-      "A.	I alone sufficient while II alone not sufficient to answer",
-      "B.	II alone sufficient while I alone not sufficient to answer",
-      "C.	Either I or II alone sufficient to answer",
-      "D.	Both I and II are not sufficient to answer",
-      "E.	Both I and II are necessary to answer"
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
     ]
   },
     {
     numb: 3,
-    questionA: "	In a shower, 5 cm of rain falls. The volume of water that falls on 1.5 hectares of ground is:",
-    answerA: " Option B",
+    questionA: "What does PHP stand for?",
+    answerA: "Hypertext Preprocessor",
     options: [
-      "A.	75 cu. m",
-      "B.	750 cu. m",
-      "C.	7500 cu. m",
-      "D.	75000 cu. m"
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
     ]
   },
     {
     numb: 4,
-    questionA: "A motorboat, whose speed in 15 km/hr in still water goes 30 km downstream and comes back in a total of 4 hours 30 minutes. The speed of the stream (in km/hr) is:",
-    answerA: " Option B",
+    questionA: "What does SQL stand for?",
+    answerA: "Structured Query Language",
     options: [
-      "A.	2 : 1",
-      "B.	3 : 2",
-      "C.	8 : 3",
-      "D.	Cannot be determined"
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
     ]
   },
     {
     numb: 5,
-    questionA: "	Two pipes A and B can fill a cistern in 37 minutes and 45 minutes respectively. Both pipes are opened. The cistern will be filled in just half an hour, if the B is turned off after:",
-    answerA: "Option D",
+    questionA: "What does XML stand for?",
+    answerA: "eXtensible Markup Language",
     options: [
-		"A.	4 hours",
-        "B.	7 hours",
-        "C.	8 hours",
-        "D.	14 hours"
-      
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
     ]
   },
   // you can uncomment the below codes and make duplicate as more as you want to add question
